b35b502cf7dad25e5227a6be81b616af6a1825de
duongdvph14918 <duongdvph14918@fpt.edu.vn>
2024-06-20 06:54:04 +0700

    abc

---
+++ WebBanGiayBongDa1/src/main/java/com/men/WebBanGiayBongDa1Application.java
@@ -0,0 +1,13 @@
+package com.men;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBanGiayBongDa1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebBanGiayBongDa1Application.class, args);
+	}
+
+}
---
+++ WebBanGiayBongDa1/src/test/java/com/men/WebBanGiayBongDa1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.men;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBanGiayBongDa1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
